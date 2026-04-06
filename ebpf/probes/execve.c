5c6a8651ef5f3460a6879eca2f480b99076a3034
cclts <chiachiaoliu@gmail.com>
2026-04-06 17:23:50 -0500

    update

--- ebpf/probes/execve.c
+++ ebpf/probes/execve.c
@@ -12,13 +12,12 @@ int handle_execve(struct trace_event_raw_sys_enter *ctx) {
     u64 pid_tgid = bpf_get_current_pid_tgid();
     e->tgid = pid_tgid >> 32;
     e->pid = pid_tgid & 0xffffffff;
+    
     u64 uid_gid = bpf_get_current_uid_gid();
     e->uid = uid_gid & 0xffffffff;
 
     struct task_struct *task = (struct task_struct *)bpf_get_current_task();
-    struct task_struct *parent;
-    bpf_probe_read_kernel(&parent, sizeof(parent), &task->real_parent);
-    bpf_probe_read_kernel(&e->ppid, sizeof(e->ppid), &parent->tgid);
+    e->ppid = BPF_CORE_READ(task, real_parent, tgid);
 
     bpf_get_current_comm(&e->comm, sizeof(e->comm));
 
